57d5431f4047cc855c8f73d55df84e66e134cbc0
Mrfh <marklow01@126.com>
2018-12-06 13:33:17 +0800

    webpack配置冲突解决

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -56,7 +56,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
       filename: 'index.html',
       template: 'index.html',
       // inject: 'head', //true
-      favicon: '.~@/assets/img/favicon.ico', // ico图标地址
+      favicon: './static/img/favicon.ico', // ico图标地址
       // chunks: ['index'],
     }),
     new webpack.DllReferencePlugin({
