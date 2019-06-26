8243d8929bb5d2d00c3abdc1c29ae4c3b0bea5dc
Mrfh <marklow01@126.com>
2019-06-26 14:02:43 +0800

    token失效已过期

--- src/views/auth/empty/index.vue
+++ src/views/auth/empty/index.vue
@@ -61,7 +61,7 @@ export default {
         .catch(err => {
           console.log("err---------", err);
           // console.log("err.data.code-", err.data.code);
-          /* this.$router.replace("login"); */
+          this.$router.replace("login");
         });
     }
   },
